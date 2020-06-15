08257b6cfeebd9f295bc41c715501e37d833d682
John Gargalionis <johngargalionis@gmail.com>
2020-06-16 02:07:33 +1000

    Added operators.py test file

---
+++ neutrinomass/completions/operators_test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from neutrinomass.completions.operators import EFF_OPERATORS, DERIV_EFF_OPERATORS
+
+
+def test_pickle():
+    op = DERIV_EFF_OPERATORS["D1"]
+    assert sum(f.derivs for f in op.operator.fields)
