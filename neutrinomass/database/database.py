ea4336490205e15fd55261ab6a58307d2a812665
John Gargalionis <johngargalionis@gmail.com>
2020-08-20 00:39:45 +1000

    Expanded database object and added some tests

--- neutrinomass/database/database.py
+++ neutrinomass/database/database.py
@@ -8,11 +8,19 @@ from neutrinomass.completions.core import (
     Completion,
     cons_completion_field,
 )
+from neutrinomass.completions.completions import remove_equivalent_completions
+
 from networkx import Graph
 from collections import defaultdict
 import time
 from typing import Dict, List
 import re
+import numpy as np
+from sympy import prime
+from itertools import groupby
+import os
+from glob import glob
+
 
 ExoticField = cons_completion_field
 
@@ -50,7 +58,7 @@ class LazyCompletion:
 
     @property
     def quantum_numbers(self):
-        return self.head["quantum_numbers"]
+        return set(self.head["quantum_numbers"])
 
     @property
     def operator_name(self):
@@ -90,27 +98,339 @@ def read_completions(filename: str):
     return completions
 
 
+def negate_u1(string):
+    """Negates number in string, for use in `process_term` as aux. function."""
+    if string == "0":
+        return string
+    elif string[0] == "-":
+        return string[1:]
+    else:
+        return "-" + string
+
+
+def process_string_term(term: tuple):
+    """Returns a list of the sorted term and its hermitian conjugate."""
+    conj_term = []
+    for field in term:
+        if field in {"L", "H", "Q", "ub", "db", "eb"}:
+            conj_term.append(field + ".conj")
+        elif field in {"L.conj", "H.conj", "Q.conj", "ub.conj", "db.conj", "eb.conj"}:
+            conj_term.append(field[:-5])
+        else:  # exotic case
+            # interchange colour dynkin indices
+            lor, col, iso, hyp, bar = field.split(",")
+            col = "".join(reversed(col))
+            hyp = negate_u1(hyp)
+            bar = negate_u1(bar)
+
+            conj_numbers = [lor, col, iso, hyp, bar]
+            conj_string = ",".join(conj_numbers)
+            conj_term.append(conj_string)
+
+    return tuple(sorted(conj_term))
+
+
 class ModelDatabase:
-    def __init__(self, path: str):
+    def __init__(
+        self, path: str, philosophy: str = "democratic", criterion: str = "mass"
+    ):
         self.path = path
+        self.philosophy = philosophy
+        self.criterion = criterion
+        self.is_forced = False
+        self.is_ordered = False
 
-        import os
-        from glob import glob
+        assert self.philosophy in {"democratic", "stringent"}
+        assert self.criterion in {"mass", "dimension"}
 
-        print("Initialising database...")
+        # print("Initialising database...")
         filenames = glob(os.path.join(self.path, "*.dat"))
         mvdb_data = [dict(read_completions(f)) for f in filenames]
         mvdb = {k: v for d in mvdb_data for k, v in d.items()}
         self.data = mvdb
 
+        # initialise prime dictionary
+        exotics = {}
+        counter = 1
+        for k, v in self.data.items():
+            for model in v:
+                for exotic in model.quantum_numbers:
+                    if exotic not in exotics:
+                        exotics[exotic] = prime(counter)
+                        counter += 1
+
+        # dict mapping exotic field (str representation) to unique prime number
+        self.exotics_prime_dict = exotics
+
+        term_dict = {}
+        counter = 1
+        for k, v in self.data.items():
+            for model in v:
+                n_terms = len(model.head["terms"])
+                for i in range(n_terms):
+                    # sort all of the terms by side effect
+                    term = tuple(sorted(model.head["terms"][i]))
+                    model.head["terms"][i] = term
+                    if term not in term_dict:
+                        conj_term = process_string_term(term)
+                        term_dict[term] = prime(counter)
+                        term_dict[conj_term] = prime(counter)
+                        counter += 1
+
+        # dict mapping sorted tuple of strings representing interaction (and
+        # conjugate) to unique prime number
+        self.term_prime_dict = term_dict
+
+        # dict mapping operator label to neutrino-mass scale estimate
+        self.scale_dict = None
+
+        # 2D array with number of models rejected from completions of jth
+        # ordered operator because a subset features in completions of ith
+        # ordered operator
+        self.filter_data = np.zeros([len(self.data), len(self.data)])
+
+    @property
+    def is_democratic(self):
+        return self.philosophy == "democratic"
+
+    @property
+    def is_stringent(self):
+        return self.philosophy == "stringent"
+
+    @property
+    def is_mass(self):
+        return self.criterion == "mass"
+
+    @property
+    def is_dimension(self):
+        return self.criterion == "dimension"
+
     def query(self, func):
         """Function that acts on each model"""
         return {k: [m for m in v if func(m)] for k, v in self.data.items()}
 
-    def filter_seesaw(self):
+    def filter_by_query(self, func):
+        """Alter internal data from results of query by side effect"""
+        self.data = self.query(func)
+
+    @classmethod
+    def no_seesaws(cls, model):
+        """Aux. query to remove seesaw fields"""
         no_seesaws = (
             lambda m: not m.contains_field("F,00,2,0,0")
             and not m.contains_field("F,00,0,0,0")
             and not m.contains_field("S,00,2,1,0")
         )
-        return self.query(no_seesaws)
+        return no_seesaws(model)
+
+    # model number is the product of primes representing the model
+    def democratic_model_number(self, model):
+        """Product of primes representing the fields in the model"""
+        prod = 1
+        for qn in model.quantum_numbers:
+            prod *= self.exotics_prime_dict[qn]
+        return prod
+
+    def stringent_model_number(self, model):
+        """Product of primes representing the terms in the Lagrangian of the model"""
+        prod = 1
+        for term in model.head["terms"]:
+            prod *= self.term_prime_dict[term]
+        return prod
+
+    def model_number(self, model):
+        """General dispatch for model number"""
+        if self.is_democratic:
+            return self.democratic_model_number(model)
+        return self.stringent_model_number(model)
+
+    def force(self):
+        """Upgrade internal data from LazyCompletion objects to Completion objects. May
+        take a while to run, probably filter the database down a little before
+        running this.
+
+        """
+        if self.is_forced:
+            return
+
+        self.is_forced = True
+        for k, v in self.data.items():
+            self.data = {k: [m.force() for m in v]}
+
+    def remove_equivalent_completions(self):
+        """Removes equivalent completions by checking for an isomorphism between the
+        lagrangians. A much more efficient removal of equivalent models can be
+        done with `stringent_remove_equivalent_models`, which will be equivalent
+        to this function in almost all situations.
+
+        """
+        if not self.is_forced:
+            raise Exception("Need to force database first.")
+
+        for k, v in self.data.items():
+            remove_equivalent_completions(v)
+
+    def democratic_remove_equivalent_models(self):
+        """Removes duplicate models only by field content"""
+        from neutrinomass.utils.functions import remove_equivalent_nopop
+
+        def eq(x, y):
+            return self.democratic_model_number(x) == self.democratic_model_number(y)
+
+        for k, v in self.data.items():
+            self.data[k] = remove_equivalent_nopop(v, eq_func=eq)
+
+    def stringent_remove_equivalent_models(self):
+        """Removes duplicate models by interaction terms in the Lagrangian"""
+        from neutrinomass.utils.functions import remove_equivalent_nopop
+
+        def eq(x, y):
+            return self.stringent_model_number(x) == self.stringent_model_number(y)
+
+        for k, v in self.data.items():
+            self.data[k] = remove_equivalent_nopop(v, eq_func=eq)
+
+    def remove_equivalent_models(self):
+        """General dispatch function for removing equivalent models"""
+        if self.is_democratic:
+            return self.democratic_remove_equivalent_models()
+        return self.stringent_remove_equivalent_models()
+
+    def filter_model_by_mass(self, op: str, model):
+        """Remove all completions with the same or a subset of the particle content of
+        an upstream model by the neutrino-mass criterion, i.e. only keep
+        leading-order contributions to the neutrino mass.
+
+        Dispatch on filtering philosophy handled by call to `model_number`.
+
+        """
+        op_scale = self.scale_dict[op]
+        sieve = self.model_number(model)
+        for k, v in self.data.items():
+            if self.scale_dict[k] >= op_scale:
+                continue
+
+            new_v = []
+            for test_model in v:
+                if self.model_number(test_model) % sieve != 0:
+                    new_v.append(test_model)
+                    continue
+
+                ordered_op_label_list = list(self.data)
+                sieve_op_pos = ordered_op_label_list.index(op)
+                other_op_pos = ordered_op_label_list.index(k)
+                self.filter_data[sieve_op_pos][other_op_pos] += 1 / len(v)
+
+            self.data[k] = new_v
+
+    def filter_models_by_mass(self, op: str):
+        for model in self.data[op]:
+            self.filter_model_by_mass(op, model)
+
+    def filter_model_by_dimension(self, op: str, model):
+        """Remove all completions with the same or a subset of the particle content of
+        an upstream model by the dimension criterion, i.e. only keep models that
+        would imply lower dimensional operators.
+
+        Dispatch on filtering philosophy handled by call to `model_number`.
+
+        """
+        from neutrinomass.completions import EFF_OPERATORS
+        from neutrinomass.completions import DERIV_EFF_OPERATORS
+
+        ops = {**EFF_OPERATORS, **DERIV_EFF_OPERATORS}
+
+        op_dim = ops[op].mass_dimension
+        sieve = self.model_number(model)
+        for k, v in self.data.items():
+            if ops[k].mass_dimension <= op_dim:
+                continue
+
+            new_v = []
+            for test_model in v:
+                if self.model_number(test_model) % sieve != 0:
+                    new_v.append(test_model)
+                    continue
+
+                ordered_op_label_list = list(self.data)
+                sieve_op_pos = ordered_op_label_list.index(op)
+                other_op_pos = ordered_op_label_list.index(k)
+                self.filter_data[sieve_op_pos][other_op_pos] += 1 / len(v)
+
+            self.data[k] = new_v
+
+    def filter_models_by_dimension(self, op: str):
+        for model in self.data[op]:
+            self.filter_model_by_dimension(op, model)
+
+    def filter_by_dimension(self):
+        for op in self.data:
+            self.filter_models_by_dimension(op)
+
+    def filter(self):
+        """Filter dispatch on filtering criterion"""
+        if not self.is_ordered:
+            self.order()
+
+        if self.is_mass:
+            func = lambda x: self.filter_model_by_mass(x)
+        else:
+            func = lambda x: self.filter_model_by_dimension(x)
+
+        for op in self.data:
+            func(op)
+
+    def order_by_mass(self):
+        """Provides `scale_dict` and orders the data dictionary by neutrino mass scale
+        prediction.
+
+        """
+        from neutrinomass.database import neutrino_mass_estimate
+        from neutrinomass.database import numerical_np_scale_estimate
+        from neutrinomass.completions import EFF_OPERATORS
+        from neutrinomass.completions import DERIV_EFF_OPERATORS
+
+        def scale_pred(op):
+            return max(
+                numerical_np_scale_estimate(i) for i in neutrino_mass_estimate(op)
+            )
+
+        ops = {**EFF_OPERATORS, **DERIV_EFF_OPERATORS}
+        scales = {k: scale_pred(v) for k, v in ops.items() if k in self.data}
+        mv_ordered = dict(reversed(sorted(scales.items(), key=lambda x: x[1])))
+
+        self.scale_dict = {k: v for k, v in mv_ordered.items()}
+        self.data = {k: self.data[k] for k, v in mv_ordered.items()}
+        self.is_ordered = True
+
+    def order_by_dimension(self):
+        """Orders the data dictionary by operator dimension"""
+        from neutrinomass.completions import EFF_OPERATORS
+        from neutrinomass.completions import DERIV_EFF_OPERATORS
+
+        ops = {**EFF_OPERATORS, **DERIV_EFF_OPERATORS}
+        sorted_data = sorted(
+            self.data.items(), key=lambda kv: ops[kv[0]].mass_dimension
+        )
+
+        self.data = dict(sorted_data)
+        self.is_ordered = True
+
+    def order(self):
+        """General dispatch on order by filtering criterion"""
+        if self.is_mass:
+            self.order_by_mass()
+        else:
+            self.order_by_dimension()
+
+    def process(self, filter_seesaws=False):
+        """All common preprocessing steps in one function"""
+        if filter_seesaws:
+            # print("Removing seesaw fields...")
+            self.filter(ModelDatabase.no_seesaws)
+        # print("Removing equivalent models...")
+        self.remove_equivalent_models()
+        # print("Ordering by neutrino-mass scales...")
+        self.order()
+        # print("Filtering democratically by neutrino-mass estimate...")
+        self.filter()
--- neutrinomass/database/database_test.py
+++ neutrinomass/database/database_test.py
@@ -1,19 +1,33 @@
 #!/usr/bin/env python3
 
-from neutrinomass.database import *
+from neutrinomass.database.database import *
 
 
 def test_query():
     db_path = "/Users/johngargalionis/Desktop/operators/"
     mvdb = ModelDatabase(db_path)
 
-    no_seesaws = (
-        lambda m: not m.contains_field("F,00,2,0,0")
-        and not m.contains_field("F,00,0,0,0")
-        and not m.contains_field("S,00,2,1,0")
-    )
-
     no_scalars = lambda m: not m.contains_field("S,*")
 
-    for k, v in mvdb.query(lambda m: no_scalars(m) and no_seesaws(m)).items():
+    func = lambda m: no_scalars(m) and ModelDatabase.no_seesaws(m)
+    for k, v in mvdb.query(func).items():
         assert not v
+
+
+def test_process_string_term():
+    test_terms = [
+        ["L.conj", "F,10,3,1/6,1", "F,10,3,7/6,1"],
+        ["F,11,1,1/2,0", "F,11,2,0,0", "F,20,0,1/3,2", "F,20,1,5/6,2"],
+        ["Q", "S,02,1,7/6,-2", "S,02,2,5/3,-2"],
+        ["S,10,0,2/3,1", "S,11,0,1,0", "S,11,1,1/2,0"],
+    ]
+    conj_terms = [
+        ["L", "F,01,3,-1/6,-1", "F,01,3,-7/6,-1"],
+        ["F,11,1,-1/2,0", "F,11,2,0,0", "F,02,0,-1/3,-2", "F,02,1,-5/6,-2"],
+        ["Q.conj", "S,20,1,-7/6,2", "S,20,2,-5/3,2"],
+        ["S,01,0,-2/3,-1", "S,11,0,-1,0", "S,11,1,-1/2,0"],
+    ]
+    proc_terms = [process_string_term(t) for t in test_terms]
+
+    for i, sorted_conj in enumerate(proc_terms):
+        assert list(sorted_conj) == sorted(conj_terms[i])
